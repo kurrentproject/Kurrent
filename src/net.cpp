10de2d1f9816a4345f5406c0103899502f47fe18
kurrentproject <dyork654@yahoo.com>
2016-12-10 16:25:07 -0800

    modified:   src/makefile.mingw 	modified:   src/makefile.unix 	modified:   src/net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-	{ "192.200.23.86", "192.200.23.86" },
+	{ "212.24.107.99", "212.24.107.99" },
     {NULL, NULL}
 };
 
